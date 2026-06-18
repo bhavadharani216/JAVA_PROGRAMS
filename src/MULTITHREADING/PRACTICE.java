63a0a00766804ed2a2b4e41b6ba5f9c5c680afcf
bhavadharani216 <bhavadharani216@gmail.com>
2026-06-18 15:03:51 +0530

    added Multithreading code

---
+++ src/MULTITHREADING/PRACTICE.java
@@ -0,0 +1,10 @@
+package MULTITHREADING;
+
+public class PRACTICE {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/set/treeset1.java
+++ src/set/treeset1.java
@@ -1,3 +1,4 @@
+
 //ascending order 
 
 package set;
